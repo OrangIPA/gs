b3949dfa6201ea68242aac03a9e1d02e20f1e2a0
Bahana Alana Khansa <alanabahana102@gmail.com>
2025-02-10 00:10:21 +0700

    delete player

--- src/state.c
+++ src/state.c
@@ -1,6 +1,7 @@
 #include "state.h"
 
 #include <stdlib.h>
+#include <string.h>
 
 State create_state() {
     State state;
@@ -8,7 +9,7 @@ State create_state() {
     state.p_count = 0;
     state.p_cap = 1;
     state.pfd_list = malloc(sizeof(int));
-    state.ppos_list = malloc(sizeof(float[2]));
+    state.ppos_list = malloc(sizeof(float) * 2);
 }
 
 void state_newplayer(State* state, int sockfd) {
@@ -24,9 +25,37 @@ void state_newplayer(State* state, int sockfd) {
 
         state->p_cap = new_cap;
         state->pfd_list = realloc(state->pfd_list, sizeof(int) * new_cap);
-        state->ppos_list = realloc(state->ppos_list, sizeof(int[2]) * new_cap);
+        state->ppos_list = realloc(state->ppos_list, sizeof(float) * 2 * new_cap);
     }
 
     state->pfd_list[state->p_count] = sockfd;
-    *state->ppos_list[state->p_count] = *(float[2]){0.0, 0.0};
+    state->ppos_list[state->p_count * 2] = 0;
+    state->ppos_list[state->p_count * 2 + 1] = 0;
+}
+
+int state_deleteplayer(State* state, int sockfd) {
+    state->p_count -= 1;
+    int index = -1;
+    for (int i = 0; i < state->p_count; i++) {
+        if (state->pfd_list[i] == sockfd) {
+            index = i;
+        }
+    }
+    if (index == -1) {
+        return -1;
+    }
+
+    for (int i = index; i < state->p_count; i++) {
+        if (i == state->p_count - 1) {
+            state->pfd_list[state->p_count - 1] = 0;
+            state->ppos_list[(state->p_count * 2)] = 0;
+            continue;
+        }
+
+        state->pfd_list[i]  = state->pfd_list[i + 1];
+        state->ppos_list[i * 2] = state->ppos_list[(i + 1) * 2];
+        state->ppos_list[i * 2 + 1] = state->ppos_list[(i + 1) * 2 + 1];
+    }
+
+    return 0;
 }
--- src/state.h
+++ src/state.h
@@ -1,16 +1,18 @@
 #ifndef STATE_H
 #define STATE_H
 
+// TODO: move pfd and ppos to array of player instead
 typedef struct state
 {
     int p_count;
     unsigned int p_cap;
     int *pfd_list;
-    float (*ppos_list)[2];
+    float *ppos_list;
 } State;
 
 State create_state();
 
 void state_newplayer(State* state, int sockfd);
+int state_deleteplayer(State* state, int sockfd);
 
 #endif
