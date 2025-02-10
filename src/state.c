2a0511bff5d2dd37ccd7f71bd4f52305609e9e55
Bahana Alana Khansa <alanabahana102@gmail.com>
2025-02-11 01:58:29 +0700

    add rwlock, move player to it's own struct with sockfd and pos, uncrustify and cmakelists config

--- src/state.c
+++ src/state.c
@@ -2,42 +2,48 @@
 
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
 
 State create_state() {
     State state;
 
     state.p_count = 0;
-    state.p_cap = 1;
-    state.pfd_list = malloc(sizeof(int));
-    state.ppos_list = malloc(sizeof(float) * 2);
+    state.p_cap   = 1;
+    state.players = malloc(sizeof(Player));
+
+    pthread_rwlock_init(&state.lock, NULL);
 }
 
-void state_newplayer(State* state, int sockfd) {
+void state_newplayer(State *state, int sockfd) {
+    pthread_rwlock_wrlock(&state->lock);
     state->p_count += 1;
 
-    // If p_count is bigger than capacity, allocate more capacity
-    // Specifically, 2^n
+    /**
+     * If p_count is bigger than capacity, allocate more capacity
+     * Specifically, 2^n
+     */
     if (state->p_count > state->p_cap) {
         unsigned int new_cap = 1;
         while (new_cap < state->p_count) {
             new_cap *= 2;
         }
 
-        state->p_cap = new_cap;
-        state->pfd_list = realloc(state->pfd_list, sizeof(int) * new_cap);
-        state->ppos_list = realloc(state->ppos_list, sizeof(float) * 2 * new_cap);
+        state->p_cap   = new_cap;
+        state->players = realloc(state->players, sizeof(Player) * state->p_cap);
     }
 
-    state->pfd_list[state->p_count] = sockfd;
-    state->ppos_list[state->p_count * 2] = 0;
-    state->ppos_list[state->p_count * 2 + 1] = 0;
+    state->players[state->p_count].pfd     = 0;
+    state->players[state->p_count].ppos[0] = 0;
+    state->players[state->p_count].ppos[1] = 0;
+    pthread_rwlock_unlock(&state->lock);
 }
 
-int state_deleteplayer(State* state, int sockfd) {
+int state_deleteplayer(State *state, int sockfd) {
+    pthread_rwlock_wrlock(&state->lock);
     state->p_count -= 1;
     int index = -1;
     for (int i = 0; i < state->p_count; i++) {
-        if (state->pfd_list[i] == sockfd) {
+        if (state->players[i].pfd == sockfd) {
             index = i;
         }
     }
@@ -47,15 +53,15 @@ int state_deleteplayer(State* state, int sockfd) {
 
     for (int i = index; i < state->p_count; i++) {
         if (i == state->p_count - 1) {
-            state->pfd_list[state->p_count - 1] = 0;
-            state->ppos_list[(state->p_count * 2)] = 0;
+            state->players[state->p_count - 1].pfd     = 0;
+            state->players[state->p_count - 1].ppos[0] = 0;
+            state->players[state->p_count - 1].ppos[1] = 0;
             continue;
         }
 
-        state->pfd_list[i]  = state->pfd_list[i + 1];
-        state->ppos_list[i * 2] = state->ppos_list[(i + 1) * 2];
-        state->ppos_list[i * 2 + 1] = state->ppos_list[(i + 1) * 2 + 1];
+        state->players[i] = state->players[i + 1];
     }
 
+    pthread_rwlock_unlock(&state->lock);
     return 0;
 }
--- src/state.h
+++ src/state.h
@@ -1,18 +1,27 @@
 #ifndef STATE_H
 #define STATE_H
 
-// TODO: move pfd and ppos to array of player instead
-typedef struct state
-{
-    int p_count;
-    unsigned int p_cap;
-    int *pfd_list;
-    float *ppos_list;
+#include <pthread.h>
+#include <stdio.h>
+
+typedef struct Player;
+
+typedef struct state {
+    int              p_count;
+    unsigned int     p_cap;
+    Player *         players;
+
+    pthread_rwlock_t lock;
 } State;
+
+typedef struct player {
+    int   pfd;
+    float ppos[2];
+} Player;
 
 State create_state();
 
-void state_newplayer(State* state, int sockfd);
-int state_deleteplayer(State* state, int sockfd);
+void state_newplayer(State *state, int sockfd);
+int state_deleteplayer(State *state, int sockfd);
 
 #endif
