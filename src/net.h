436f66e3851543f16bc55045be8c21eb2bd348bd
Bahana Alana Khansa <alanabahana102@gmail.com>
2025-07-31 00:20:49 +0700

    update the protocol beyond 255 message length

--- src/net.h
+++ src/net.h
@@ -5,17 +5,17 @@
 
 #define CLIENT_DISCONNECT -2
 
-int send_message(int fd, uint8_t *message, size_t len);
+int send_message(int fd, size_t len, uint8_t *message);
 
 /**
  * @brief  Retrive tcp messages by TLV protocol without type
  *
  * The protocol is, each message have length, and body.
- * The first bytes is the length and the rest is the body.
- *
- * since max value of 8 bit uint is 255 (0xFF), message length can't exceed 248 (0xF8)
- * bytes 248 (0xF8) and above is reserved for futureproofing (i'm thinking of doing it utf-8 style)
- *
+ * If the first byte is less than 252 (0xFC) then the first byte is the length
+ * If the first byte equal to 253 (0xFD) then the following 2 byte is the length
+ * If the first byte equal to 254 (0xFE) then the following 4 byte is the length
+ * 255 (0xFF) is reserved
+ * 
  * ## example
  * ```
  * 0    1    2    3    4    5    6    7    8    9    A    ..   F
@@ -26,6 +26,16 @@ int send_message(int fd, uint8_t *message, size_t len);
  * ```
  * Above stream contains 3 messages:
  * `0x7F28` with length 0x02, `0x01` with length 0x01, and `0x003F9A9045..F4` with length 0xA0
+ * 
+ * ```
+ * 0    1    2    3    4    5    6    7    8    9    A    ..   0113
+ * +----+----+----+----+----+----+----+----+----+----+----+----+----+----+
+ * | FC | 01 | 10 | 01 | 01 | A0 | 00 | 3F | 9A | 90 | 45 | .. | F4 | .. |
+ * +----+----+----+----+----+----+----+----+----+----+----+----+----+----+
+ * Tag  Len  Len  Dat  Dat  Dat  Dat  Dat  Dat  Dat  Dat  ..   Dat
+ * ```
+ * Above stream contains 1 messages:
+ * `0x0101A0003F9A9045..F4` with the length of `0x0110`
  *
  * @param fd socket file descriptor for the tcp connection
  * @param[out] out_message return owned uint8_t array pointer
